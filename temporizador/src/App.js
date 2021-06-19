7d0a068ac9ece75c3c2026be10911d1e70a6f352
ZathKiwo <gui_souza_123@hotmail.com>
2021-06-18 22:48:32 -0300

    Timer feito no react

---
+++ temporizador/src/App.js
@@ -0,0 +1,10 @@
+import './App.scss';
+import Timer from './Components/Timer'
+
+function App() {
+  return (
+    <Timer />
+  );
+}
+
+export default App;
---
+++ temporizador/src/Components/Timer.js
@@ -0,0 +1,53 @@
+import { React, Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+import logo from './cursor.png'
+import relogio from './relogio.png'
+
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        this.soma = 1;
+        this.angle = 6;
+        this.timeValue = null
+        this.state = {
+            value: 60
+        }
+    }
+
+    countdown() {
+        this.timeValue = setInterval(() => {
+            this.setState({
+                value : this.state.value - this.soma
+            }) 
+            document.querySelector("#img").style.transform = `rotate(${this.angle}deg)`;
+            this.angle = this.angle + 6;
+            if (this.state.value <= 0) {
+                clearInterval(this.timeValue);
+            }
+            }, 1000);
+    }
+
+    stopCount() {
+        clearInterval(this.timeValue);
+    }
+
+    render () {
+        console.log(this.state.value)
+        return (
+            <div className='buttons'>
+                <div className='relogio-completo'>
+                    <img className='relogio' src={relogio} alt='aaa'/>
+                    <img id='img' className='ponteiro' src={logo} alt='aaa'/>
+                </div>              
+                <h1>{this.state.value}</h1>
+                <Button className='count' variant='success' onClick={() => this.countdown()}>Countdown</Button>
+                <Button className='stop' variant='danger' onClick={() => this.stopCount()}>Parar Contagem</Button>
+                
+            </div>
+        )
+    }
+}
+
+export default Timer;
